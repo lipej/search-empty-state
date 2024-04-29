7dc8ed8e1b1ae2c22038b6e6c319b959dd72d2a2
Felipe José <dasilvalipejose@gmail.com>
2024-04-29 10:47:01 -0300

    Initial Commit

---
+++ SearchEmptyState/SearchEmptyStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchEmptyStateApp.swift
+//  SearchEmptyState
+//
+//  Created by Felipe José on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SearchEmptyStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
